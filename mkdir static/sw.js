3cf24c14399a0f7c0ce66c6e9e31e080112a5ff6
lagostotop <lagostotop@gmail.com>
2026-07-08 05:22:00 +0100

    Add files via upload

---
+++ mkdir static/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'brain30-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/static/manifest.json',
+        '/static/icon-192.png',
+        '/static/icon-512.png',
+        '/static/apple-touch-icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
