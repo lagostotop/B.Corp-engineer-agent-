bed466445755ef702fb068465ad9e66b2f535931
lagostotop <lagostotop@gmail.com>
2026-07-14 01:35:12 +0100

    Add service worker for asset caching
    Implement service worker for caching and fetching assets.

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,36 @@
+const CACHE_NAME = 'brain3-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icon-180.png',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
 
+// Install - cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch - serve from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Update cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
+  );
+});
