970547d05ca2a0b782fe64bb7f30e448910bb21f
lagostotop <lagostotop@gmail.com>
2026-09-23 00:56:32 +0100

    Create __init__.py

---
+++ tools/__init__.py
@@ -0,0 +1,6 @@
+from .registry import ToolRegistry
+from .calculator import CalculatorTool
+from .file_search import FileSearchTool
+from .web_search import WebSearchTool
+
+__all__=["ToolRegistry","CalculatorTool","FileSearchTool","WebSearchTool"]
