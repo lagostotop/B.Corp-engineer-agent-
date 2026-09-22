1e7cf2baeb27f5e346880a5534ea15eacbbfc38f
lagostotop <lagostotop@gmail.com>
2026-09-23 00:56:01 +0100

    Delete *init*.py

--- tools/*init*.py
+++
@@ -1,6 +0,0 @@
-from .registry import ToolRegistry
-from .web_search import WebSearchTool
-from .calculator import CalculatorTool
-from .file_search import FileSearchTool
-
-__all__=["ToolRegistry","WebSearchTool","CalculatorTool","FileSearchTool"]# Tools package
